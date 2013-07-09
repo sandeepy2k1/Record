1d452b08d653c48a4330911f7dae3b5b8d1431b2
Amit Suneja <IDS@temp-my.local>
2013-07-09 16:22:26 +0530

    Initial Commit

---
+++ Record/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Record
+//
+//  Created by Amit Suneja on 09/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Record/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Record
+//
+//  Created by Amit Suneja on 09/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
